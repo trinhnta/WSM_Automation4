9eba6325e31272d7a7f6dcdaaa0a374193ac3f22
hangnt22 <nguyen.thi.hang@framgia.com>
2018-10-30 09:02:11 +0700

    Hangnt nop bai tap lan 2

--- selenium/src/test/java/Project_WSM/WSM_PERSONAL_REQUEST.java
+++ selenium/src/test/java/Project_WSM/WSM_PERSONAL_REQUEST.java
@@ -2,11 +2,13 @@ package Project_WSM;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class WSM_PERSONAL_REQUEST extends WSM {
@@ -94,14 +96,14 @@ public class WSM_PERSONAL_REQUEST extends WSM {
     void PER_REQ_OT_006() throws InterruptedException {
         PER_REQ_OT_003();
         actualText = driver.findElement(By.xpath(staff_nameTextbox)).getAttribute("value");
-        Assert.assertEquals(actualText, InformationTest.staff_name);
+        Assert.assertEquals(actualText, InformationTest.staffName);
     }
 
     @Test(priority = 06)
     void PER_REQ_OT_007() throws InterruptedException {
         PER_REQ_OT_003();
         actualText = driver.findElement(By.xpath(staff_codeTextbox)).getAttribute("value");
-        Assert.assertEquals(actualText, InformationTest.staff_code);
+        Assert.assertEquals(actualText, InformationTest.staffCode);
     }
 
     @Test(priority = 07)
@@ -110,11 +112,288 @@ public class WSM_PERSONAL_REQUEST extends WSM {
         actualText = driver.findElement(By.xpath(branchTextbox)).getText();
         Assert.assertEquals(actualText, InformationTest.branch);
     }
+
     @Test(priority = 8)
     void PER_REQ_OT_009() throws InterruptedException {
         PER_REQ_OT_003();
-        actualText = driver.findElement(By.xpath(groupTextbox)).getText();
-        Assert.assertEquals(actualText, InformationTest.group);
+        clickAction(checkboxOT);
+        wait_element(groupDropDown);
+        actual = check_visibleElement(groupDropDown);
+        Assert.assertEquals(actual, expect);
+    }
+
+    @Test(priority = 9)
+    void PER_REQ_OT_010() throws InterruptedException {
+        PER_REQ_OT_003();
+        clickAction(checkboxOT);
+        wait_element(groupDropDown);
+        clickAction(groupDropDown);
+        wait_element(optionDropDown);
+        List<WebElement> group = driver.findElements(By.xpath(optionDropDown));
+        actualText = group.get(0).getText();
+        Assert.assertEquals(actualText, InformationTest.group[1]);
+        actualText = group.get(1).getText();
+        Assert.assertEquals(actualText, InformationTest.group[2]);
+        actualText = group.get(2).getText();
+        Assert.assertEquals(actualText, InformationTest.group[3]);
+        actualText = group.get(3).getText();
+        Assert.assertEquals(actualText, InformationTest.group[4]);
+        actualText = group.get(4).getText();
+        Assert.assertEquals(actualText, InformationTest.group[5]);
+        actualText = group.get(5).getText();
+        Assert.assertEquals(actualText, InformationTest.group[6]);
+        actualText = group.get(6).getText();
+        Assert.assertEquals(actualText, InformationTest.group[7]);
+        actualText = group.get(7).getText();
+        Assert.assertEquals(actualText, InformationTest.group[8]);
+        actualText = group.get(8).getText();
+        Assert.assertEquals(actualText, InformationTest.group[9]);
+    }
+
+    @Test(priority = 10)
+    void PER_REQ_OT_011() throws InterruptedException {
+        PER_REQ_OT_003();
+        send_keys(projectTextbox, InformationTest.datatest11[0]);
+        send_keys(fromTextbox, InformationTest.datatest11[1]);
+        clickAction(toTextbox);
+        sleep(1);
+        clearText(toTextbox);
+        send_keys(toTextbox, InformationTest.datatest11[2]);
+        send_keys(reasonTextbox, InformationTest.datatest11[3]);
+        clickAction(saveBtn);
+        wait_TimeOut();
+        Assert.assertEquals(gettext(employeeXpath), InformationTest.ouput11[0]);
+        Assert.assertEquals(gettext(staffNameXpath), InformationTest.ouput11[1]);
+        Assert.assertEquals(gettext(dateOfCreationXpath), InformationTest.ouput11[2]);
+        Assert.assertEquals(gettext(fromXpath), InformationTest.ouput11[3]);
+        Assert.assertEquals(gettext(toXpath), InformationTest.ouput11[4]);
+        Assert.assertEquals(gettext(projectNameXpath), InformationTest.ouput11[5]);
+        Assert.assertEquals(gettext(reasonXpath), InformationTest.ouput11[6]);
+        Assert.assertEquals(gettext(statusXpath), InformationTest.ouput11[7]);
+        Assert.assertEquals(gettext(beingHandledByXpath), InformationTest.ouput11[8]);
+        Assert.assertEquals(gettext(requestHoursXpath), InformationTest.ouput11[9]);
+    }
+
+    @Test(priority = 11)
+    void PER_REQ_OT_012() throws InterruptedException {
+        PER_REQ_OT_003();
+        send_keys(projectTextbox, InformationTest.datatest12[0]);
+        clickAction(checkboxOT);
+        send_keys(fromTextbox, InformationTest.datatest12[1]);
+        clickAction(toTextbox);
+        sleep(1);
+        clearText(toTextbox);
+        send_keys(toTextbox, InformationTest.datatest12[2]);
+        send_keys(reasonTextbox, InformationTest.datatest12[3]);
+        clickAction(saveBtn);
+        wait_TimeOut();
+        Assert.assertEquals(gettext(employeeXpath), InformationTest.ouput12[0]);
+        Assert.assertEquals(gettext(staffNameXpath), InformationTest.ouput12[1]);
+        Assert.assertEquals(gettext(dateOfCreationXpath), InformationTest.ouput12[2]);
+        Assert.assertEquals(gettext(fromXpath), InformationTest.ouput12[3]);
+        Assert.assertEquals(gettext(toXpath), InformationTest.ouput12[4]);
+        Assert.assertEquals(gettext(projectNameXpath), InformationTest.ouput12[5]);
+        Assert.assertEquals(gettext(reasonXpath), InformationTest.ouput12[6]);
+        Assert.assertEquals(gettext(statusXpath), InformationTest.ouput12[7]);
+        Assert.assertEquals(gettext(beingHandledByXpath), InformationTest.ouput12[8]);
+        Assert.assertEquals(gettext(requestHoursXpath), InformationTest.ouput12[9]);
+    }
+
+    @Test(priority = 12)
+    void PER_REQ_OT_013() throws InterruptedException {
+        PER_REQ_OT_003();
+        send_keys(projectTextbox, InformationTest.datatest13[0]);
+        send_keys(fromTextbox, InformationTest.datatest13[1]);
+        clickAction(toTextbox);
+        sleep(1);
+        clearText(toTextbox);
+        send_keys(toTextbox, InformationTest.datatest13[2]);
+        send_keys(reasonTextbox, InformationTest.datatest13[3]);
+        clickAction(saveBtn);
+        wait_TimeOut();
+        Assert.assertEquals(gettext(employeeXpath), InformationTest.ouput13[0]);
+        Assert.assertEquals(gettext(staffNameXpath), InformationTest.ouput13[1]);
+        Assert.assertEquals(gettext(dateOfCreationXpath), InformationTest.ouput13[2]);
+        Assert.assertEquals(gettext(fromXpath), InformationTest.ouput13[3]);
+        Assert.assertEquals(gettext(toXpath), InformationTest.ouput13[4]);
+        Assert.assertEquals(gettext(projectNameXpath), InformationTest.ouput13[5]);
+        Assert.assertEquals(gettext(reasonXpath), InformationTest.ouput13[6]);
+        Assert.assertEquals(gettext(statusXpath), InformationTest.ouput13[7]);
+        Assert.assertEquals(gettext(beingHandledByXpath), InformationTest.ouput13[8]);
+        Assert.assertEquals(gettext(requestHoursXpath), InformationTest.ouput13[9]);
+    }
+
+    @Test(priority = 13)
+    void PER_REQ_OT_014() throws InterruptedException {
+        PER_REQ_OT_003();
+        send_keys(projectTextbox, InformationTest.datatest14[0]);
+        send_keys(fromTextbox, InformationTest.datatest14[1]);
+        clickAction(toTextbox);
+        sleep(1);
+        clearText(toTextbox);
+        send_keys(toTextbox, InformationTest.datatest14[2]);
+        send_keys(reasonTextbox, InformationTest.datatest14[3]);
+        clickAction(saveBtn);
+        wait_TimeOut();
+        Assert.assertEquals(gettext(employeeXpath), InformationTest.ouput14[0]);
+        Assert.assertEquals(gettext(staffNameXpath), InformationTest.ouput14[1]);
+        Assert.assertEquals(gettext(dateOfCreationXpath), InformationTest.ouput14[2]);
+        Assert.assertEquals(gettext(fromXpath), InformationTest.ouput14[3]);
+        Assert.assertEquals(gettext(toXpath), InformationTest.ouput14[4]);
+        Assert.assertEquals(gettext(projectNameXpath), InformationTest.ouput14[5]);
+        Assert.assertEquals(gettext(reasonXpath), InformationTest.ouput14[6]);
+        Assert.assertEquals(gettext(statusXpath), InformationTest.ouput14[7]);
+        Assert.assertEquals(gettext(beingHandledByXpath), InformationTest.ouput14[8]);
+        Assert.assertEquals(gettext(requestHoursXpath), InformationTest.ouput14[9]);
+    }
+
+    @Test(priority = 14)
+    void PER_REQ_OT_015() throws InterruptedException {
+        PER_REQ_OT_003();
+        send_keys(projectTextbox, InformationTest.datatest15[0]);
+        send_keys(fromTextbox, InformationTest.datatest15[1]);
+        clickAction(toTextbox);
+        sleep(1);
+        clearText(toTextbox);
+        send_keys(toTextbox, InformationTest.datatest15[2]);
+        send_keys(reasonTextbox, InformationTest.datatest15[3]);
+        clickAction(saveBtn);
+        wait_TimeOut();
+        wait_element(actual01);
+        actual = check_visibleElement(actual01);
+        Assert.assertEquals(actual, expect);
+
+    }
+
+    @Test(priority = 15)
+    void PER_REQ_OT_016() throws InterruptedException {
+        PER_REQ_OT_003();
+        send_keys(projectTextbox, InformationTest.datatest16[0]);
+        send_keys(fromTextbox, InformationTest.datatest16[1]);
+        clickAction(toTextbox);
+        sleep(1);
+        clearText(toTextbox);
+        send_keys(toTextbox, InformationTest.datatest16[2]);
+        send_keys(reasonTextbox, InformationTest.datatest16[3]);
+        clickAction(saveBtn);
+        wait_TimeOut();
+        wait_element(actual01);
+        actualText = gettext(fromXpath);
+        Assert.assertEquals(actualText, InformationTest.output16);
+
+    }
+
+    @Test(priority = 16)
+    void PER_REQ_OT_017() throws InterruptedException {
+        PER_REQ_OT_003();
+        send_keys(projectTextbox, InformationTest.datatest17[0]);
+        send_keys(fromTextbox, InformationTest.datatest17[1]);
+        clickAction(toTextbox);
+        sleep(1);
+        clearText(toTextbox);
+        send_keys(toTextbox, InformationTest.datatest17[2]);
+        send_keys(reasonTextbox, InformationTest.datatest17[3]);
+        clickAction(saveBtn);
+        wait_TimeOut();
+        Assert.assertEquals(gettext(employeeXpath), InformationTest.ouput17[0]);
+        Assert.assertEquals(gettext(staffNameXpath), InformationTest.ouput17[1]);
+        Assert.assertEquals(gettext(dateOfCreationXpath), InformationTest.ouput17[2]);
+        Assert.assertEquals(gettext(fromXpath), InformationTest.ouput17[3]);
+        Assert.assertEquals(gettext(toXpath), InformationTest.ouput17[4]);
+        Assert.assertEquals(gettext(projectNameXpath), InformationTest.ouput17[5]);
+        Assert.assertEquals(gettext(reasonXpath), InformationTest.ouput17[6]);
+        Assert.assertEquals(gettext(statusXpath), InformationTest.ouput17[7]);
+        Assert.assertEquals(gettext(beingHandledByXpath), InformationTest.ouput17[8]);
+        Assert.assertEquals(gettext(requestHoursXpath), InformationTest.ouput17[9]);
+    }
+
+    @Test(priority = 17)
+    void PER_REQ_OT_018() throws InterruptedException {
+        PER_REQ_OT_003();
+        send_keys(projectTextbox, InformationTest.datatest18[0]);
+        send_keys(fromTextbox, InformationTest.datatest18[1]);
+        clickAction(toTextbox);
+        sleep(1);
+        clearText(toTextbox);
+        send_keys(toTextbox, InformationTest.datatest18[2]);
+        send_keys(reasonTextbox, InformationTest.datatest18[3]);
+        clickAction(saveBtn);
+        wait_element(error);
+        actual = check_visibleElement(error);
+        Assert.assertEquals(actual, expect);
+        actualText = gettext(error);
+        Assert.assertEquals(actualText, InformationTest.msgOutput18);
+        sleep(5);
+
+    }
+
+    @Test(priority = 18)
+    void PER_REQ_OT_019() throws InterruptedException {
+        PER_REQ_OT_003();
+        send_keys(projectTextbox, InformationTest.datatest19[0]);
+        send_keys(fromTextbox, InformationTest.datatest19[1]);
+        clickAction(toTextbox);
+        sleep(1);
+        clearText(toTextbox);
+        send_keys(toTextbox, InformationTest.datatest19[2]);
+        send_keys(reasonTextbox, InformationTest.datatest19[3]);
+        clickAction(saveBtn);
+        wait_element(errorProjectName);
+        actual = check_visibleElement(errorProjectName);
+        Assert.assertEquals(actual, expect);
+        actualText = gettext(errorProjectName);
+        Assert.assertEquals(actualText, InformationTest.msgOutput19);
+        sleep(5);
+
+    }
+
+    @Test(priority = 19)
+    void PER_REQ_OT_020() throws InterruptedException {
+        PER_REQ_OT_003();
+        send_keys(projectTextbox, InformationTest.datatest20[0]);
+        send_keys(toTextbox, InformationTest.datatest20[2]);
+        send_keys(reasonTextbox, InformationTest.datatest20[3]);
+        clickAction(saveBtn);
+        wait_element(errorFrom);
+        actual = check_visibleElement(errorFrom);
+        Assert.assertEquals(actual, expect);
+        actualText = gettext(errorFrom);
+        Assert.assertEquals(actualText, InformationTest.msgOutput20);
+        sleep(5);
+    }
+
+    @Test(priority = 20)
+    void PER_REQ_OT_021() throws InterruptedException {
+        PER_REQ_OT_003();
+        send_keys(projectTextbox, InformationTest.datatest21[0]);
+        send_keys(fromTextbox, InformationTest.datatest21[1]);
+        send_keys(reasonTextbox, InformationTest.datatest21[3]);
+        clickAction(saveBtn);
+        wait_element(errorTo);
+        actual = check_visibleElement(errorTo);
+        Assert.assertEquals(actual, expect);
+        actualText = gettext(errorTo);
+        Assert.assertEquals(actualText, InformationTest.msgOutput21);
+        sleep(5);
+    }
+
+    @Test(priority = 21)
+    void PER_REQ_OT_022() throws InterruptedException {
+        PER_REQ_OT_003();
+        send_keys(projectTextbox, InformationTest.datatest22[0]);
+        send_keys(fromTextbox, InformationTest.datatest22[1]);
+        clickAction(toTextbox);
+        sleep(1);
+        clearText(toTextbox);
+        send_keys(toTextbox, InformationTest.datatest22[2]);
+        clickAction(saveBtn);
+        wait_element(errorReason);
+        actual = check_visibleElement(errorReason);
+        Assert.assertEquals(actual, expect);
+        actualText = gettext(errorReason);
+        Assert.assertEquals(actualText, InformationTest.msgOutput22);
+        sleep(5);
+
     }
 
     @BeforeMethod
